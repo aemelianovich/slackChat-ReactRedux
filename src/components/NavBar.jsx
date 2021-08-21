cb778c50fe422d07d362e1eb7dd8473e7b27eb78
Aleskey Emelianovich <aemelianovich@yandex.ru>
2021-08-21 18:02:02 -0500

    tenth step1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useUserContext } from './UserContext.jsx';
 
-const NavBar = () => {
+const NavBar = (props) => {
   const { user, setUser } = useUserContext();
   const { t } = useTranslation();
 
@@ -17,7 +17,13 @@ const NavBar = () => {
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
-        <a className="navbar-brand" href="/">{t('navBar.title')}</a>
+        <a
+          className="navbar-brand"
+          href="/"
+          onClick={(e) => { e.preventDefault(); location = e.target.href; }}
+        >
+          {t('navBar.title')}
+        </a>
         { user ? (<Button variant="primary" onClick={handleClick}>{t('navBar.exit')}</Button>) : null}
       </div>
     </nav>
