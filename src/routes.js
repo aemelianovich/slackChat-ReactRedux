eddee5be7df3e9a9975b23be6eb3e5d678655272
Aleskey Emelianovich <aemelianovich@yandex.ru>
2021-08-21 21:34:08 -0500

    tenth step1

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,8 @@
 // @ts-check
 
-// const host = (process.env.NODE_ENV === 'production') ? 'https://ae-chat-slack.herokuapp.com' : null;
 const prefix = 'api/v1';
 
 export default {
-  // host,
   chatDataPath: () => [prefix, 'data'].join('/'),
   channelsPath: () => [prefix, 'channels'].join('/'),
   channelPath: (id) => [prefix, 'channels', id].join('/'),
