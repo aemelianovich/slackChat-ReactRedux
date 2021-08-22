54a42a33c59efd64e50cd2b391cbc44dce1768ac
Aleskey Emelianovich <aemelianovich@yandex.ru>
2021-08-21 19:40:39 -0500

    tenth step1

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,8 +2,6 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Spinner } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 import { useGetChatDataQuery } from '../app/services/chatApi.js';
 import { useUserContext } from './UserContext.jsx';
 import { actions } from '../app/slices';
@@ -13,46 +11,19 @@ import Messages from './Messages.jsx';
 const Chat = () => {
   const { user } = useUserContext();
   const dispatch = useDispatch();
-  const { data, isSuccess, isError } = useGetChatDataQuery(user);
-  const { t } = useTranslation();
+  const { data, isSuccess } = useGetChatDataQuery(user);
 
   useEffect(() => {
     if (isSuccess) {
       dispatch(actions.initChatData(data));
     }
-  }, [data, dispatch]);
-
-  const render = () => {
-    if (isSuccess) {
-      return (
-        <>
-          <Channels />
-          <Messages />
-        </>
-      );
-    } if (isError) {
-      return (<div className="col text-danger align-self-center" align="center">{t('errors.generic')}</div>);
-    }
-
-    return (null
-      /* 
-      <div className="col align-self-center" align="center">
-        <Spinner
-          as="span"
-          animation="border"
-          size="md"
-          role="status"
-          aria-hidden="true"
-        />
-      </div>
-      */
-    );
-  };
+  }, [isSuccess, data, dispatch]);
 
   return (
     <div className="h-100 my-4 overflow-hidden rounded shadow container">
       <div className="row h-100 bg-white flex-md-row">
-        {render()}
+        <Channels />
+        <Messages />
       </div>
     </div>
   );
