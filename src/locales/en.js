9ad67143ddd50975ff20a24d529aa59edac03acd
Aleskey Emelianovich <aemelianovich@yandex.ru>
2021-10-12 12:31:23 -0500

    fix#6

---
+++ src/locales/en.js
@@ -0,0 +1,60 @@
+export default {
+  translation: {
+    navBar: {
+      title: 'Hexlet Chat',
+      exit: 'Exit',
+    },
+    modals: {
+      addChannelTitle: 'Add Channel',
+      removeChannelTitle: 'Remove Channel',
+      renameChannelTitle: 'Rename Channel',
+      closeBtn: 'Cancel',
+      submitBtn: 'Send',
+      removeBtn: 'Remove',
+      removeQuestion: 'Are you sure?',
+      requiredName: 'Required field',
+      uniqueName: 'Should be unique',
+      nameLength: 'From 3 to 20 symbols',
+    },
+    channels: {
+      title: 'Channels',
+      prefix: '#',
+      removeOption: 'Remove',
+      renameOption: 'Rename',
+    },
+    login: {
+      enter: 'Login',
+      username: 'Username',
+      password: 'Password',
+      noUsername: 'No account?',
+      registration: 'Registration',
+    },
+    signUp: {
+      requiredField: 'Required field',
+      usernameLength: 'From 3 to 20 symbols!',
+      passwordLength: 'No less than 6 symbols!',
+      identicalPassword: 'Passwords should be identical',
+      title: 'Registration',
+      username: 'Username',
+      password: 'Password',
+      confirmPassword: 'Confirm Password',
+      register: 'Register',
+    },
+    messages: {
+      newMessage: 'Enter a message...',
+      sendMessageBtn: 'Sent',
+      messageCount: '{{count}} message',
+      messageCount_plural: '{{count}} messages',
+    },
+    errors: {
+      invalidCredentials: 'Incorrect username or password.',
+      generic: 'Please, connect with administrator.',
+      existingUser: 'Such user already exists.',
+      sendError: 'Error during the message sending. Please try again later or connect with administrator.',
+      timeoutError: 'Network is working slow. Please try again later or connect with administrator.',
+    },
+    noMatch: {
+      title: '404 Error Code',
+    },
+  },
+};
